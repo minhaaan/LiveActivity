3b8a99acf020c23f7e8d1c61ba5154b73ea4fb18
최민한 <chmh0411@gmail.com>
2022-08-06 21:22:55 +0900

    Initial Commit

---
+++ LiveActivity/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LiveActivity
+//
+//  Created by 최민한 on 2022/08/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiveActivity/LiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityApp.swift
+//  LiveActivity
+//
+//  Created by 최민한 on 2022/08/06.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
