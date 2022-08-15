544f503a374f64a750fa4dbd4392ff03dfe3a3e7
최민한 <chmh0411@gmail.com>
2022-08-16 06:48:58 +0900

    feature: 뷰 구성

--- LiveActivity/LiveActivity/ViewController.swift
+++ LiveActivity/LiveActivity/ViewController.swift
@@ -7,13 +7,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+  
+  // MARK: Properties
+  
+  private let startButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("START", for: .normal)
+    return button
+  }()
+  
+  private let stopButton: UIButton = {
+    let button = UIButton(type: .system)
+    button.setTitle("STOP", for: .normal)
+    return button
+  }()
+  
+  // MARK: LifeCycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    setupLayout()
+    setupGesture()
   }
-
-
+  
+  // MARK: Method
+  
+  private func setupGesture() {
+    startButton.addTarget(nil, action: #selector(startButtonDidTapped), for: .touchUpInside)
+    stopButton.addTarget(nil, action: #selector(stopButtonDidTapped), for: .touchUpInside)
+  }
+  
+  @objc private func startButtonDidTapped() {
+    print("DEBUG: startButtonDidTapped")
+  }
+  
+  @objc private func stopButtonDidTapped() {
+    print("DEBUG: stopButtonDidTapped")
+  }
+  
 }
 
+// MARK: Layout
+
+extension ViewController {
+  private func setupLayout() {
+    view.backgroundColor = .white
+    
+    view.addSubview(startButton)
+    view.addSubview(stopButton)
+    
+    startButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      startButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+    ])
+    
+    stopButton.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      stopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      stopButton.topAnchor.constraint(equalTo: startButton.bottomAnchor, constant: 20)
+    ])
+  }
+  
+}
