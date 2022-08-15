29aa4198e2b1cc9095e6f2a43f7b0406e5a3cb08
최민한 <chmh0411@gmail.com>
2022-08-16 06:40:44 +0900

    프로젝트 생성

---
+++ LiveActivity/LiveActivity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveActivity
+//
+//  Created by 최민한 on 2022/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
